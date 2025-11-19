014902a2874bb250eb00429b3dadbed10f24d918
Patrick James Pangilinan <patrickjpangilinan@protonmail.com>
2025-11-19 22:33:55 +0800

    Update build.sh

--- votechain/build.sh
+++ votechain/build.sh
@@ -2,10 +2,17 @@
 # Exit on error
 set -o errexit
 
+# Explicitly set the settings module for the build process
 export DJANGO_SETTINGS_MODULE=backend.settings
 
 # 1. Install all the Python libraries from requirements.txt
 pip install -r requirements.txt
 
-# 2. Run the database migrations (create tables in the new DB)
+# 2. Collect static files (CSS, JS, Images)
+# This gathers admin files so WhiteNoise can serve them
+python manage.py collectstatic --no-input
+
+python manage.py createsuperuser --no-input --username=$DJANGO_SUPERUSER_USERNAME --email=$DJANGO_SUPERUSER_EMAIL
+
+# 3. Run the database migrations
 python manage.py migrate
